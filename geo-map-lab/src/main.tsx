12f9e77b8146c2a85667c0f94d1409029069d446
Itzik <test@example.com>
2026-03-01 08:47:33 +0200

    fix github web approach

--- geo-map-lab/src/main.tsx
+++ geo-map-lab/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+// שנה את הייבוא מ-BrowserRouter ל-HashRouter
+import { HashRouter } from 'react-router-dom';
+
+// ב-render:
+<HashRouter>
+  <App />
+</HashRouter>
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- geo-map-lab/vite.config.ts
+++ geo-map-lab/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GeoVisLab/',  // <--- ודא שהוספת את השורה הזו עם לוכסנים בשני הצדדים
+  base: '/GeoVisLab/', // חשוב מאוד! זה השם של הרפוזיטורי שלך
 })
